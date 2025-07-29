b6495b9393dc9c082e01c47395fd9a0be0e2d810
TakenHara <kavlod12345@gmail.com>
2025-07-29 13:48:16 +0300

    feat: fix borders

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -14,6 +14,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
         output: {
             filename: isDev ? 'bundle.[contenthash:4].js' :  'bundle.js' ,
             path: paths.build,
+            publicPath: '/',
             clean: true
         },
         plugins: buildPlugins(options),
